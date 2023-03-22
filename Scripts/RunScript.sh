240f163ba677069466ee3be1dd121c2f4f64cf81
Fernando Duarte <duarte@alum.mit.edu>
2023-03-22 11:47:59 -0400

    Add check and release GitHub actions

---
+++ Scripts/RunScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wolframscript -file "Scripts/${1}"
+WS_EXIT_CODE=$(echo $?)
+
+if [ $WS_EXIT_CODE -eq 139 ]
+then 
+    echo "::warning::Warning: wolframscript did not exit cleanly"
+    exit 0 
+fi
+exit $WS_EXIT_CODE
